49dc5438a14a8638c03d2801a6fe35e4904310ce
Tyler <tyler@magebinary.com>
2022-01-03 01:22:44 +0700

    SP-4053 Fix the invalid order reference issue

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -80,12 +80,14 @@ class latitude_officialpaymentModuleFrontController extends ModuleFrontControlle
             $reference  = $this->getReferenceNumber();
             // Save the reference for validation when response coming back from
             $cookie->reference = $reference;
+            $cookie->__set('lpay_reserve_order_reference', $reference);
 
             $cart       = $this->context->cart;
             $amount     = $cart->getOrderTotal();
             $customer   = $this->context->customer;
             $address    = new Address($cart->id_address_delivery);
-            $cookie->cart_amount = $amount;
+            $cookie->__set('cart_amount', $amount);
+            $cookie->__set('lpay_reserve_order_cart_id', $cart->id);
 
             $payment = array(
                 BinaryPay_Variable::REFERENCE                => (string) $reference,
@@ -118,6 +120,7 @@ class latitude_officialpaymentModuleFrontController extends ModuleFrontControlle
             }
 
             $response = $gateway->purchase($payment);
+            $cookie->__set('payment_token', $response['token'] ?? null);
             $cookie->payment_token = $response['token'] ?? null;
             $purchaseUrl = $this->module->getConfigData('paymentUrl', $response);
         } catch (BinaryPay_Exception $e) {
